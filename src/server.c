e68334668434c3a273ee76cd04b0bb483b18cb82
258ms <valeriotriolo3010@gmail.com>
2024-08-01 12:48:22 +0200

    added port used exception

--- src/server.c
+++ src/server.c
@@ -168,6 +168,11 @@ int create_http_server(t_config configfd)
 
     int sd = create_server_socket(port);
 
+    if(sd < 0) {
+        ERROR_LOG("[error] Failed to initialize server socket, exiting...");
+        return -1;
+    }
+
     while (1)
     {
         fflush(stdout);
